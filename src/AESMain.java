c6332b025ec3666d5eac4accb14fcdd49dd0d05a
Bahador <49520007+Baha2rM98@users.noreply.github.com>
2019-08-04 17:04:48 +0430

    UI system completely changed, bug fixed

--- src/AESMain.java
+++ src/AESMain.java
@@ -5,49 +5,48 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class AESMain {
+    private static File path;
+    private static String name;
+
     public static void main(String[] args) throws IOException {
-        Scanner scn = new Scanner(System.in);
         AES aes = new AES();
+        Scanner scn = new Scanner(System.in);
         System.out.println("Welcome!\nThis program uses for encrypting files!\n");
-        System.out.println("For using string mod enter 1, For using file mod enter 2, For exit enter 0.");
-        String ans = scn.next();
-        if (ans.equals("1")) {
-            while (true) {
-                System.out.println("Enter Text : (enter q or Q for exit)");
-                String text = scn.nextLine();
-                if (text.equals("q") || text.equals("Q")) {
-                    System.out.println("Terminated!");
-                    break;
-                } else {
-                    String e = aes.encryption(text);
-                    System.out.println(e);
-                    String d = aes.decryption(e);
-                    System.out.println(d + "\n");
-                }
-            }
-        }
-        if (ans.equals("2")) {
-            while (true) {
-                System.out.println("Enter the path of your file's directory (for example C:\\\\User\\\\...)\nEnter q or Q for exit");
+        while (true) {
+            System.out.println("Choose one the options below:\n1) encryption mode\n2) decryption mode\n3) exit");
+            String ans = scn.next();
+            if (ans.equals("1")) {
+                System.out.println("welcome to encryption mode!");
+                System.out.println("Enter the path of your file's directory (for example C:\\\\User\\\\...)\nEnter q to exit");
                 String way = scn.next();
-                if (way.equals("q") || way.equals("Q")) {
-                    System.out.println("Terminated!");
-                    break;
-                } else {
-                    File path = new File(way);
-                    System.out.println("Enter your file name with suffix (for example .txt .bin and...)");
-                    String name = scn.next();
-                    aes.fileEncryption(path, name);
-                    System.out.println("Do you wanna decrypt this file?\nPress y or n...");
-                    String res = scn.next();
-                    if (res.equals("y") || res.equals("Y")) {
-                        aes.fileDecryption(path, name);
-                    } else {
-                        System.out.println("Ops! you haven't chosen y!\nTerminated...");
-                        break;
-                    }
-                }
+                if (way.toLowerCase().equals("q"))
+                    exit();
+                path = new File(way);
+                System.out.println("Enter your file name with suffix (for example .txt .bin and etc)\nEnter q to exit");
+                name = scn.next();
+                if (name.toLowerCase().equals("q"))
+                    exit();
+                aes.fileEncryption(path, name);
             }
+            if (ans.equals("2")) {
+                System.out.println("welcome to decryption mode!");
+                System.out.println("Enter the path of your encrypted file's directory (for example C:\\\\User\\\\...)\nEnter q to exit");
+                String way = scn.next();
+                if (way.toLowerCase().equals("q"))
+                    exit();
+                path = new File(way);
+                System.out.println("Enter your original file name with suffix (for example .txt .bin and etc)\nEnter q to exit");
+                name = scn.next();
+                if (name.toLowerCase().equals("q"))
+                    exit();
+                aes.fileDecryption(path, name);
+            }
+            if (ans.equals("3"))
+                exit();
         }
+    }
+
+    private static void exit() {
+        System.exit(-1);
     }
 }
